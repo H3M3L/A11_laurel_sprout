a1bcd1b6c9801c1fdffd92434615121b28cc4145
argraur <argraur@gmail.com>
2020-01-25 22:27:42 +0530

    laurel_sprout: Use Pixel charger animation
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -438,6 +438,11 @@ PRODUCT_COPY_FILES += \
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+    
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
 
 # QCOM
 PRODUCT_COPY_FILES += \
